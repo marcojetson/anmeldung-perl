9d871fd94b117a37c3dbf35939c41bc3557d32ce
marcojetson <marcojetson@gmail.com>
2015-03-08 01:56:55 +0100

    add district filter and beep count options

--- README.md
+++ README.md
@@ -2,4 +2,25 @@
 I tried to get an appointment for the Anmeldung for almost a month till I got frustrated and created this little script.
 
 ## Usage
-    ./anmeldung.pl [--maxdays 14] [--delay 60]
+    ./anmeldung.pl [options...]
+
+### Options
+    --district DISTRICT  Limit search to a specific district (default all)
+    --maxdays DAYS       Set the maximum days till the appointment (default 14)
+    --delay SECONDS      Delay between fetches (default 60)
+    --beeps COUNT        Number of beeps to play when available date found (default 7)
+    --list-districts     Displays the districts list
+
+### Districts
+     1: Charlottenburg - Wilmersdorf
+     2: Friedrichshain - Kreuzberg
+     3: Lichtenberg
+     4: Marzahn - Hellersdorf
+     5: Mitte
+     6: Neukölln
+     7: Pankow
+     8: Reinickendorf
+     9: Spandau
+    10: Steglitz - Zehlendorf
+    11: Tempelhof - Schöneberg
+    12: Treptow - Köpenick
--- anmeldung.pl
+++ anmeldung.pl
@@ -2,37 +2,86 @@
 
 use strict;
 use warnings;
+
 use DateTime;
 use Getopt::Long;
 use LWP::UserAgent;
-use Term::ANSIColor;
-use Time::localtime;
 use XML::LibXML;
 
-my $maxdays = 14,
+my $district = 0;
+my $maxdays = 14;
 my $delay = 60;
+my $beeps = 7;
+my $list_districts = 0;
+my $help = 0;
 
 GetOptions(
-    "maxdays=i" => \$maxdays,
-    "delay=i" => \$delay,
+    'district=i' => \$district,
+    'maxdays=i' => \$maxdays,
+    'delay=i' => \$delay,
+    'beeps=i' => \$beeps,
+    'list-districts' => \$list_districts,
+    'help' => \$help,
 );
 
-my $url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleisterlist=122210,122217,122219,122227,122231,122243,122252,122260,122262,122254,122271,122273,122277,122280,122282,122284,122291,122285,122286,122296,150230,122301,122297,122294,122312,122314,122304,122311,122309,317869,324433,325341,324434,122281,324414,122283,122279,122276,122274,122267,122246,122251,122257,324395,122208,122226&anliegen[]=120686";
+my @districts = (
+    [122210, 122217, 122219, 122227, 122231, 122243, 122252, 122260, 122262, 122254, 122271, 122273, 122277, 122280, 122282, 122284, 122291, 122285, 122286, 122296, 150230, 122301, 122297, 122294, 122312, 122314, 122304, 122311, 122309, 317869, 324433, 325341, 324434, 122281, 324414, 122283, 122279, 122276, 122274, 122267, 122246, 122251, 122257, 324395, 122208, 122226],
+    [122210, 122217, 122219, 122227],
+    [122231, 122243],
+    [122252, 122260, 122262, 122254],
+    [122271, 122273, 122277],
+    [122280, 122282, 122284],
+    [122291, 122285, 122286, 122296],
+    [150230, 122301, 122297, 122294],
+    [122312, 122314, 122304, 122311, 122309, 317869, 324433, 325341, 324434],
+    [122281, 324414, 122283, 122279],
+    [122276, 122274, 122267],
+    [122246, 122251, 122257, 324395],
+    [122208, 122226],
+);
 
-my $today = DateTime->today();
+sub help {
+    print 'Usage: anmeldung.pl [options...]', "\n",
+          'Options:', "\n",
+          ' --district DISTRICT  Limit search to a specific district (default all)', "\n",
+          ' --maxdays DAYS       Set the maximum days till the appointment (default 14)', "\n",
+          ' --delay SECONDS      Delay between fetches (default 60)', "\n",
+          ' --beeps COUNT        Number of beeps to play when available date found (default 7)', "\n",
+          ' --list-districts     Displays the districts list', "\n",
+          ' --help               Shows this message', "\n";
 
-sub writeln {
-    my $message = shift;
-    print colored("[" . $today->dmy(".") . "]", "GREY15"), " ", $message, "\n";
+    exit 0;
+}
+
+sub districts {
+    print 'Districts:', "\n",
+          ' 1: Charlottenburg - Wilmersdorf', "\n",
+          ' 2: Friedrichshain - Kreuzberg', "\n",
+          ' 3: Lichtenberg', "\n",
+          ' 4: Marzahn - Hellersdorf', "\n",
+          ' 5: Mitte', "\n",
+          ' 6: Neukölln', "\n",
+          ' 7: Pankow', "\n",
+          ' 8: Reinickendorf', "\n",
+          ' 9: Spandau', "\n",
+          '10: Steglitz - Zehlendorf', "\n",
+          '11: Tempelhof - Schöneberg', "\n",
+          '12: Treptow - Köpenick', "\n";
+
+    exit 0;
 }
 
 sub crawl {
-    my $result = 0;
+    my $found = 0;
+
+    my $now = DateTime->today();
+
+    my $url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleisterlist=' . join(',', @{$districts[$district]}) . '&anliegen[]=120686';
 
     my $ua = LWP::UserAgent->new(ssl_opts => {verify_hostname => 0});
     my $response = $ua->get($url);
-    if ($response->status_line ne "200 OK") {
-        return $result;
+    if ($response->status_line ne '200 OK') {
+        return;
     }
 
     my $parser = XML::LibXML->new(recover => 2);
@@ -42,31 +91,37 @@ sub crawl {
     my @free_days = $doc->findnodes('//a[@class="tagesauswahl"]');
 
     for my $free_day (@free_days) {
-        my $link = $free_day->getAttribute("href");
+        my $link = $free_day->getAttribute('href');
         if ($link =~ /datum=([0-9]{4})-([0-9]{2})-([0-9]{2})/) {
             my $date = DateTime->new(year => $1, month => $2, day => $3);
-            if ($date->delta_days($today)->in_units("days") <= $maxdays) {
-                print colored("Available date on " . $date->dmy("."), "bold green"), "\n";
-                print colored("https://service.berlin.de/terminvereinbarung/termin/" . $link, "GREY5"), "\n";
+            if ($date->delta_days($now)->in_units('days') <= $maxdays) {
+                print 'Available date on ', $date->dmy('.'), ': ', "\n",
+                      'https://service.berlin.de/terminvereinbarung/termin/', $link, "\n",
+                      '-', "\n";
 
-                $result = 1;
+                $found = 1;
             }
         }
     }
 
-    return $result;
+    if ($found) {
+        print "\a" x $beeps;
+
+        exit 0;
+    }
+}
+
+if ($help) {
+    help();
+}
+
+if ((not defined $districts[$district]) || $list_districts) {
+    districts();
 }
 
 for (;;) {
-    writeln(colored("Fetching...", "GREY15"));
-
-    if (crawl()) {
-        print "\a\a\a\a\a";
-        last;
-    }
-
-    writeln(colored("Noting found, retrying in " . $delay . " seconds", "GREY15"));
+    print 'Fetching...', "\n";
+    crawl();
+    print 'Nothing found, retrying in ', $delay, ' seconds', "\n";
     sleep($delay);
 }
-
-exit 0;
